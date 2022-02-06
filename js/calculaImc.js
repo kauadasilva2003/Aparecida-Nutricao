bb9da6227d6dff34de3f6c4e3f408fecc2e70af6
Kauã Silva <kauad4235@gmail.com>
2022-02-05 21:42:45 -0300

    Validação do formulario adicionada.

--- js/calculaImc.js
+++ js/calculaImc.js
@@ -15,16 +15,16 @@ for (var i = 0; i < pacientes.length; i++) {
 
   var tdImc = paciente.querySelector(".info-imc");
 
-  var pesoEhValido = true;
-  var alturaEhValida = true;
+  var pesoEhValido =validaPeso(peso);
+  var alturaEhValida = validaAltura(altura)
 
-  if (peso <= 0 || peso >= 1000) {
+  if (!pesoEhValido) {
     pesoEhValido = false;
     tdImc.textContent = "Peso Inválido";
     paciente.classList.add("paciente-invalido");
   }
 
-  if (altura <= 0 || altura >= 3.0) {
+  if (!alturaEhValida) {
     alturaEhValida = false;
     tdImc.textContent = "Altura Inválida";
     paciente.classList.add("paciente-invalido");
@@ -33,6 +33,22 @@ for (var i = 0; i < pacientes.length; i++) {
   if (pesoEhValido && alturaEhValida) {
     var imc = calculaImc(peso, altura);
     tdImc.textContent = imc;
+  }
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso <= 1000){
+      return true
+    }else{
+      return false;
+    }
+}
+
+function validaAltura(altura){
+  if(altura >= 0 && altura <=3.00 ){
+    return true
+  }else{
+    return false;
   }
 }
 
